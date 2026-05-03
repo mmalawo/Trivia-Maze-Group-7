2974e4c690c92daebb3d30ad783af2932c09bee1
Tifanie <tifngo457@gmail.com>
2026-05-03 11:50:02 -0700

    Middle of working on SettingsView.java

--- src/view/SettingsView.java
+++ src/view/SettingsView.java
@@ -1,4 +1,35 @@
 package view;
 
+import javax.swing.*;
+
 public class SettingsView {
+    public SettingsView(){
+        JPanel settingsPanel = new JPanel();
+        panel.setLayout
+        settingsPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        settingsPanel.setTitle("Trivia Maze - Settings");
+
+
+        settingsPanel.pack(); // Causes this window to be sized to fit preferred size in gamepanel
+        settingsPanel.setLocationRelativeTo(null); // Puts it at the center
+        settingsPanel.setVisible(true);
+    }
+
 }
+
+// Full Screen / Windowed
+// Sound Volume / Toggled
+// Brightness? / Dark mode?
+
+        // Initialize window
+       // window.setResizable(false);
+       // window.setUndecorated(false); // For Fullscreen mode, make 'true'
+
+
+        // Show menuView
+      //  GameMenuView menuView = new GameMenuView();
+      //  MenuController menuController = new MenuController(menuView);
+      //  window.add(menuView);
+
+
+
