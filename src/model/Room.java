5dbc6d29d0348c85f418a9e814aec53af8994081
Makani Malawo <mmalawo@uw.edu>
2026-05-02 17:05:50 -0700

    Updated Room class to connect with Door objects

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,30 +1,36 @@
-
 public class Room {
+    private Door northDoor;
+    private Door southDoor;
+    private Door eastDoor;
+    private Door westDoor;
+    private boolean visited;
     private int doors;
     private boolean hasLeft;
     private boolean hasRight;
     private boolean hasCenter;
 
     public Room() {
+        this.northDoor = new Door();
+        this.southDoor = new Door();
+        this.eastDoor = new Door();
+        this.westDoor = new Door();
+        this.visited = false;
         this.doors = 0;
         this.hasLeft = false;
         this.hasRight = false;
         this.hasCenter = false;
     }
 
-    public int getDoors() {
-        return doors;
-    }
+    public Door getNorthDoor() { return northDoor; }
+    public Door getSouthDoor() { return southDoor; }
+    public Door getEastDoor() { return eastDoor; }
+    public Door getWestDoor() { return westDoor; }
 
-    public boolean canGoLeft() {
-        return hasLeft;
-    }
+    public boolean isVisited() { return visited; }
+    public void setVisited(boolean visited) { this.visited = visited; }
 
-    public boolean canGoRight() {
-        return hasRight;
-    }
-
-    public boolean canGoCenter() {
-        return hasCenter;
-    }
+    public int getDoors() { return doors; }
+    public boolean canGoLeft() { return hasLeft; }
+    public boolean canGoRight() { return hasRight; }
+    public boolean canGoCenter() { return hasCenter; }
 }
